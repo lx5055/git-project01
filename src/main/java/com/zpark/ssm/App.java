0240309e7d335867d414bc47daf6560b3e00db7f
lx <lx@163.com>
2023-05-06 09:05:37 +0800

    lx hotfix first commit！

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -2,7 +2,8 @@ package com.zpark.ssm;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Git！");
-        System.out.println("Hello Git！ lx master second commit!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git! lx master second commit!");
+        System.out.println("Hello Git! lx hotfix first commit!");
     }
 }
